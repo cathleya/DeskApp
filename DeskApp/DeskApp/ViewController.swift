dc980fbddc2ef6c49e059e5e7b8bcc8161570b2b
cathleya <cathleya2016@gmail.com>
2016-12-03 14:53:44 +0800

    initial project
    created a new project

---
+++ DeskApp/DeskApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DeskApp
+//
+//  Created by herry on 03/12/2016.
+//  Copyright © 2016 herry. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
